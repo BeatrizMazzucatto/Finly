d1cda23cd87d67bf15401a73b19d184b782eaeba
BeatrizMazzucatto <beatrizmazzucatto@gmail.com>
2026-06-02 21:18:14 -0300

    Correção de bugs e melhorias

--- Finly/components/ui/index.ts
+++ Finly/components/ui/index.ts
@@ -2,5 +2,4 @@ export { Button } from "./Button";
 export { Card } from "./Card";
 export { Chip } from "./Chip";
 export { Input } from "./Input";
-export { ProgressBar } from "./ProgressBar";
 export { TransactionItem } from "./TransactionItem";
